d3ae3d818a1cc2d71502ea79e44a919cd3d11867
YuriVictoria <105010586+YuriVictoria@users.noreply.github.com>
2023-12-05 15:19:06 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,60 +1,73 @@
-//Carrega a biblioteca do sensor ultrassonico
+//Carrega a biblioteca do sensor ultrassonico e do servo
 #include <Ultrasonic.h>
 #include <Servo.h>
  
-//Define os pinos para o trigger e echo
+//Define os pinos para o trigger e echo do sensor ultrassonico e da tampa
 #define PINO_TRIGGER_TAMPA 4
 #define PINO_ECHO_TAMPA 5
-
 #define PINO_TRIGGER_CAPACIDADE 6
 #define PINO_ECHO_CAPACIDADE 7
 
+//Define pino do servo
 #define PINO_SERVO 9
 
-#define LED 3
+//Define pino do led
+#define PINO_LED 3
 
-//Inicializa o sensor nos pinos definidos acima
+//Inicializa os sensores nos pinos definidos acima
 Ultrasonic tampa(PINO_TRIGGER_TAMPA, PINO_ECHO_TAMPA);;
-
 Ultrasonic capacidade(PINO_TRIGGER_CAPACIDADE, PINO_ECHO_CAPACIDADE);
 
-Servo s;
+//Inicializa o servo
 int pos = 0;
+Servo s;
 
 void setup()
 {
-  //para o led RGB Ânodo comum
-  // Funciona no 0
-  pinMode(LED, OUTPUT);
-
-  Serial.begin(9600);
+  pinMode(PINO_LED, OUTPUT);
   s.attach(PINO_SERVO);
   s.write(pos);
 }
  
 void loop()
 {
-  //Le as informacoes do sensor, em cm
-  float sensorTampa;
-  long microsec = tampa.timing();
-  
-  sensorTampa = tampa.convert(microsec, Ultrasonic::CM);
-  
   estadoCapacidade();
+  
+  //Le as informacoes do sensor em cm
+  long microsecTampa = tampa.timing();
+  float sensorTampa = tampa.convert(microsecTampa, Ultrasonic::CM);
 
-  if(sensorTampa <= 5 && !estadoTampa())
+  if(sensorTampa <= 5 && !abertaTampa())
     abrirTampa();
 
-  else if(sensorTampa <= 5 && estadoTampa())
+  else if(sensorTampa <= 5 && abertaTampa())
     fecharTampa();
+}
+
+float capacidadeEmCm(){
+  long microsecCapacidade = capacidade.timing();
+  return capacidade.convert(microsecCapacidade, Ultrasonic::CM);
+}
+
+void estadoCapacidade(){
+  if(capacidadeEmCm() < 10)
+    analogWrite(PINO_LED, HIGH);
+  else
+    analogWrite(PINO_LED, LOW);
+}
+
+int abertaTampa() {
+  if(s.read() == 0)
+    return 0; //Fechada
+  else
+    return 1; //Aberta
 }
 
 void abrirTampa() {
   for(pos; pos <= 180; pos++) {
     s.write(pos);
     delay(15);
-  }
-    
+  }    
   delay(3000);
 }
 
@@ -63,28 +76,5 @@ void fecharTampa() {
     s.write(pos);
     delay(15);
   }
-  
   delay(3000);
-}
-
-int estadoTampa() {
-  if(s.read() == 0)
-    return 0;
-
-  else
-    return 1;
-}
-
-float capacidadeEmCm(){
-  long sec = capacidade.timing();
-  float sensorCapacidade;
-  sensorCapacidade = capacidade.convert(sec, Ultrasonic::CM);
-  return sensorCapacidade;
-}
-
-void estadoCapacidade(){
-  if(capacidadeEmCm() < 10)
-    analogWrite(LED, HIGH);
-  else
-    analogWrite(LED, LOW);
 }
